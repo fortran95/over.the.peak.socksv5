754ec2c2bee768c4b962a7172b84b839b4e8db90
NeoAtlantis <rijndael@yeah.net>
2014-01-04 23:42:21 +0800

    test pass

--- util/xmpp.tunneler.js
+++ util/xmpp.tunneler.js
@@ -3,16 +3,97 @@
  */
 var sockets = {};
 
+function socket(from, id, xmpp){
+    var self = this;
+    var state = 0, socket = new $.nodejs.net.Socket();
+
+    function send(data){
+        data.id = id;
+        xmpp.send(from, JSON.stringify(data));
+    };
+
+    socket.on('error', function(e){
+        send({
+            err: e.message,
+        });
+        socket.destroy();
+        state = -1;
+    });
+
+    socket.on('data', function(data){
+        send({
+            res: 'data',
+            data: data.toString('base64'),
+        });
+    });
+
+    socket.on('end', function(data){
+        if(undefined == data) return;
+        send({
+            res: 'end',
+            data: data.toString('base64'),
+        });
+    });
+
+    socket.on('close', function(){
+        state = -1;
+        send({
+            res: 'close',
+        });
+    });
+
+    this.receive = function(json){
+        switch(state){
+            case 0:
+                if('connect' != json.cmd || 0 != state)
+                    return send({
+                        err: 1
+                    });
+
+                var port = parseInt(json.port), addr = json.addr;
+
+                socket.connect(port, addr, function(){
+                    send({
+                        res: 'connect',
+                    });
+
+                    state = 1;
+                });
+                break;
+            case 1:
+                var data;
+
+                if('end' == json.cmd){
+                    data = new $.nodejs.buffer.Buffer(json.data, 'base64');
+                    socket.end(data);                    
+                };
+
+                if('data' == json.cmd){
+                    data = new $.nodejs.buffer.Buffer(json.data, 'base64');
+                    socket.write(data);
+                };
+
+            default:
+                break;
+        };
+    };
+
+    return this;
+};
+
+//////////////////////////////////////////////////////////////////////////////
+
 function handle(from, json){
     var jid = new $.nodejs.xmpp.JID(from).bare().toString();
     var id = parseInt(json.id);
     if(isNaN(id)) return;
 
     if(undefined == sockets[jid]) sockets[jid] = {};
-    if(undefined == sockets[jid][id]) 
-        sockets[jid][id] = new $.nodejs.net.Socket();
+    if(undefined == sockets[jid][id]){ 
+        sockets[jid][id] = new socket(from, id, xmpp);
+    };
 
-
+    sockets[jid][id].receive(json);
 };
 
 //////////////////////////////////////////////////////////////////////////////
@@ -26,11 +107,11 @@ if(process.argv.length < 4){
 var jid = process.argv[2],
     password = process.argv[3];
 
-var xmpp = $.xmpp(jid, password);
+xmpp = $.xmpp(jid, password);
 
 xmpp.on('data', function(data){
     try{
-        var json = JSON.parse(data);
+        var json = JSON.parse(data.content);
         handle(data.from, json);
     } catch(e){
     };
