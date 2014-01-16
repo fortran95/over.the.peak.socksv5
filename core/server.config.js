824edd0ea901b8104c23cff1bf650cbf4dc239a7
NeoAtlantis <rijndael@yeah.net>
2014-01-16 23:45:40 +0800

    save

--- core/server.config.js
+++
@@ -1,8 +0,0 @@
-var server = $.nodejs.http.createServer(function(req, res){
-    res.end('You have hit the configuration panel.');
-});
-
-// start server
-server.listen($.config['manage-port']);
-
-module.exports = server;
--- core/server.proxy.js
+++
@@ -1,8 +0,0 @@
-var server = $.socksv5(
-    $.config['service-port'],
-    //$.proxys.dummy,
-    $.proxys.xmpp(process.argv[2], process.argv[3], process.argv[4]),
-    $.analyzer
-);
-
-module.exports = server;
