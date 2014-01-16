e1664168b8dcc31deaa3d3b86efafd9edc01d4bd
NeoAtlantis <rijndael@yeah.net>
2014-01-16 20:08:36 +0800

    doing, NOT TESTED

--- start.js
+++ start.js
@@ -1,6 +1,7 @@
 require('./lib/baum.js');
 
 $.config = JSON.parse($.nodejs.fs.readFileSync('./config'));
+$.streaming = require('./lib/streaming.js');
 //////////////////////////////////////////////////////////////////////////////
 
 // set up proxy
