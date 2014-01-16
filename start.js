acf04856883bc15c988416eca094479b349c01c1
NeoAtlantis <rijndael@yeah.net>
2014-01-16 22:23:22 +0800

    debugging

--- start.js
+++ start.js
@@ -5,5 +5,24 @@ $.streaming = require('./lib/streaming.js');
 //////////////////////////////////////////////////////////////////////////////
 
 // set up proxy
-var serverProxy = require('./core/server.proxy.js'),
-    serverConfig = require('./core/server.config.js');
+var mechanism = require('./lib/mechanism/__init__.js')(),
+    proxy = require('./lib/proxy/__init__.js')();
+
+if($.config.client){
+    var proxyClient = proxy.client[$.config.client.proxy.select](),
+        mechanismClient = mechanism.client[$.config.client.mechanism.select]()
+    ;
+
+    for(var key in $.config.client.proxy.config)
+        proxyClient.configure(key, $.config.client.proxy.config[key]);
+    for(var key in $.config.client.mechanism.config)
+        mechanismClient.configure(key, $.config.client.mechanism.config[key]);
+
+    mechanismClient.start();
+
+    proxyClient.setMechanism(mechanismClient);
+    proxyClient.start();
+};
+
+if($.config.server){
+};
